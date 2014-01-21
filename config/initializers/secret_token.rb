523b65bf19e6b77fbc9f7f3ad9d99bd89355e991
Andy <abaudin@gmail.com>
2014-01-21 23:09:41 +0100

    migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Events::Application.config.secret_key_base = 'dc45bfef13a6b88c4d896a5b470512aef3216bd2970befd3ce98c5a03b702906b1a9e67a205bcaa1f44f35fb248e6909712a938905698dd1ac314a93d91235bc'
---
+++ db/migrate/20140121201939_add_fields_to_events.rb
@@ -0,0 +1,5 @@
+class AddFieldsToEvents < ActiveRecord::Migration
+  def change
+    add_column :events, :description, :text
+  end
+end
